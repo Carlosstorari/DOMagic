8cab88fbe15ff1955db9343a2fcc089ea51c9bd3
carlosStorari <carlos.storari95@gmail.com>
2023-12-21 20:44:59 -0300

    implementing navigation logic on bottom navigation

--- app/src/main/java/com/project/catalogingmtgcards/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/project/catalogingmtgcards/presentation/ui/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import com.project.catalogingmtgcards.R
 import com.project.catalogingmtgcards.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var navController: NavController
@@ -19,6 +19,20 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.main_activity_nav_host) as NavHostFragment
 
         navController = navHostFragment.navController
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.search_page -> {
+                    navController.navigate(R.id.action_createdDecksFragment_to_searchCardFragment)
+                    true
+                }
+                R.id.deck_page -> {
+                    navController.navigate(R.id.action_searchCardFragment_to_createdDecksFragment)
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 
